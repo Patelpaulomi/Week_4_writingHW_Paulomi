91a377a4d9a3d1b5334c6d217a7737862011c4bc
Paulomi Patel <mital.mital1312>
2023-04-07 00:01:51 +0100

    Week 4 writing HW

---
+++ src/programme_22_multilevel_inheritage/Animal.java
@@ -0,0 +1,7 @@
+package programme_22_multilevel_inheritage;
+
+public class Animal {
+    public void eat() {
+        System.out.println("eating...");
+    }
+}
---
+++ src/programme_22_multilevel_inheritage/BabyDog.java
@@ -0,0 +1,7 @@
+package programme_22_multilevel_inheritage;
+
+public class BabyDog extends Dog{
+    public void weep() {
+        System.out.println("weeping...");
+    }
+}
